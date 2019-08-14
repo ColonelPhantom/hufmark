993dceec7001398da6c3f1f412b4362958ef1cd8
Quinten Kock <quintenkock@gmail.com>
2019-08-14 04:23:51 +0200

    Always display as char

--- src/history.rs
+++ src/history.rs
@@ -37,11 +37,16 @@ impl<T> std::ops::Index<usize> for History<T> {
         &self.h[idx]
     }
 }
-impl<T: std::fmt::Display + Clone> std::fmt::Display for History<T> {
+impl<T: std::fmt::Display + Clone + Into<char>> std::fmt::Display for History<T> {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         let mut v = self.h.as_slice().to_vec();
         v.reverse();
-        for c in v {
+        for item in v {
+            let c = item.into();
+            if c == '\n' {
+                write!(f, " ")?;
+                continue;
+            }
             write!(f, "{}", c)?;
         }
         Ok(())
