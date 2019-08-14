5357e73bac94c8276b7b9ef757a4f07911ba967b
Quinten Kock <quintenkock@gmail.com>
2019-08-14 03:45:14 +0200

    Make everything generic

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,35 @@
 mod markov;
 mod history;
 
+use history::History;
+use markov::Markov;
+
+pub type HistoryType = char;
+
+
 fn main() {
-    let mut hist = history::History::new(3);
-    for c in "Hello World!".chars() {
+    let args: Vec<String> = std::env::args().collect();
+    
+    let mut hist: History<u8> = History::new(3);
+    let mut markov: Markov<u8> = Markov::<u8>::new();
+    let mut correct = 0;
+    let mut wrong = 0;
+
+    let text = std::fs::read(&args[1]).unwrap();
+
+    for c in text {
+        let prediction = markov.predict(&hist);
+        let prediction_trimmed: Vec<_> = prediction.into_iter().take(2).collect();
+        // println!("{} ({}) -> {:?}", hist, c, prediction_trimmed);
+        match prediction_trimmed.get(0) {
+            Some((pc, _val)) => match *pc == c {
+                true => correct += 1,
+                false => wrong += 1,
+            }
+            None => {}
+        }
+        markov.train(&hist, c);
         hist.push(c);
-        println!("{:?}", hist);
     }
+    println!("Correct {}, wrong {}", correct, wrong);
 }
--- src/markov.rs
+++ src/markov.rs
@@ -1,42 +1,72 @@
 use std::collections::HashMap;
+use std::hash::Hash;
 use super::history::History;
 
-type HistoryType = char;
-type PredictType = HistoryType;
-
-pub const HISTORY_LEN: usize = 16;
-pub fn history_fac(ago: usize) -> u32 {
-    2u32.pow((HISTORY_LEN - ago) as u32)
+fn len_fac(len: usize) -> u32 {
+    len.pow(2) as u32
 }
 
 
-#[derive(Clone, Default)]
-struct MarkovValue {
-    possibilities: HashMap<char, u32>,
+
+#[derive(Clone)]
+struct MarkovValue<T: Clone+Eq+Hash> {
+    possibilities: HashMap<T, u32>,
 }
-impl MarkovValue {
+impl<T: Clone+Eq+Hash> MarkovValue<T> {
     fn new() -> Self {
         return Self {
             possibilities: HashMap::new()
         }
     }
-    fn train(&mut self, outcome: PredictType) {
+    fn train(&mut self, outcome: T) {
         *self.possibilities.entry(outcome).or_default() += 1;
     }
 }
-impl std::ops::AddAssign for MarkovValue {
+impl<T: Clone+Eq+Hash> std::ops::AddAssign for MarkovValue<T> {
     fn add_assign(&mut self, rhs: Self) {
         for (key, lik) in rhs.possibilities {
             *self.possibilities.entry(key).or_insert(0) += lik;
         }
     }
 }
-impl std::ops::Add for MarkovValue {
+impl<T: Clone+Eq+Hash> std::ops::Add for MarkovValue<T> {
     type Output = Self;
     fn add(self, rhs: Self) -> Self {
         let mut new = self.clone();
         new += rhs;
         new
+    }
+}
+impl<T: Clone+Eq+Hash+Copy> std::ops::AddAssign<&Self> for MarkovValue<T> {
+    fn add_assign(&mut self, rhs: &Self) {
+        for (key, lik) in &rhs.possibilities {
+            *self.possibilities.entry(*key).or_insert(0) += lik;
+        }
+    }
+}
+impl<T: Clone+Eq+Hash+Copy> std::ops::Add<&Self> for MarkovValue<T> {
+    type Output = Self;
+    fn add(self, rhs: &Self) -> Self {
+        let mut new = self.clone();
+        new += rhs;
+        new
+    }
+}
+impl<T: Clone+Eq+Hash+Copy> std::ops::Mul<u32> for &MarkovValue<T> {
+    type Output = MarkovValue<T>;
+    fn mul(self, rhs: u32) -> MarkovValue<T> {
+        let new: HashMap<T, u32> = self.possibilities.iter()
+                .map(|(key, val)| {
+                    (*key, val * rhs) 
+                }).collect();
+        MarkovValue {
+            possibilities: new,
+        }
+    }
+}
+impl<T: Clone+Eq+Hash> std::default::Default for MarkovValue<T> {
+    fn default() -> Self {
+        Self::new()
     }
 }
 
@@ -45,20 +75,41 @@ impl std::ops::Add for MarkovValue {
 //     values: [char; HISTORY_LEN],
 // }
 
-type MarkovKey = Vec<HistoryType>;
+type MarkovKey<T> = Vec<T>;
+pub type PredictType<T> = T;
 
-pub struct Markov {
-    hist: HashMap<MarkovKey, MarkovValue>,
+
+pub type Prediction<T> = Vec<(PredictType<T>, u32)>;
+
+pub struct Markov<T: Clone+Eq+Hash> {
+    hist: HashMap<MarkovKey<T>, MarkovValue<T>>,
 }
-impl Markov {
+impl<T: Clone+Eq+Hash+Copy> Markov<T> {
     pub fn new() -> Self {
         Self {hist: HashMap::new()}
     }
-    pub fn train(&mut self, past: History<char>, outcome: char) {
+    pub fn train(&mut self, past: &History<T>, outcome: T) {
         // TODO: train based on older data (not just last character)
         for i in 0..past.cur_len() {
             let h = past.get_slice(i).to_vec();
             self.hist.entry(h).or_default().train(outcome);
         }
+    }
+    pub fn predict(&self, past: &History<T>) -> Prediction<T> {
+        let mut p = MarkovValue::new();
+        // TODO: predict based on stuff thats longer ago
+        for i in 0..past.cur_len() {
+            let h = past.get_slice(i).to_vec();
+            match self.hist.get(&h) {
+                Some(m) => p += m * len_fac(i),
+                None => {}
+            }
+        }
+        let mut v: Prediction<T> = p.possibilities.into_iter()
+                .filter(|(_, val)| *val > 0)
+                .collect();
+        // The wrong order is so it sorts descending
+        v.sort_by(|(_, a), (_, b)| b.partial_cmp(a).unwrap());
+        v
     }
 }
