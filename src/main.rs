77d4802f1044ff99b196d1df8c0bd0d5853b7eb5
Quinten Kock <quintenkock@gmail.com>
2019-08-14 04:24:12 +0200

    Increase hist size,  other main() improvs

--- src/main.rs
+++ src/main.rs
@@ -10,17 +10,19 @@ pub type HistoryType = char;
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     
-    let mut hist: History<u8> = History::new(3);
-    let mut markov: Markov<u8> = Markov::<u8>::new();
+    let mut hist = History::new(20);
+    let mut markov = Markov::new();
     let mut correct = 0;
     let mut wrong = 0;
 
-    let text = std::fs::read(&args[1]).unwrap();
+    let text: Vec<_> = std::fs::read(&args[1]).unwrap().into_iter().map(|c| c as char).collect();
+    // let text_str = std::fs::read_to_string(&args[1]).unwrap();
+    // let text = text_str.chars();
 
     for c in text {
         let prediction = markov.predict(&hist);
         let prediction_trimmed: Vec<_> = prediction.into_iter().take(2).collect();
-        // println!("{} ({}) -> {:?}", hist, c, prediction_trimmed);
+        // println!("{} ({}) -> {:?}", hist, c as char, prediction_trimmed);
         match prediction_trimmed.get(0) {
             Some((pc, _val)) => match *pc == c {
                 true => correct += 1,
