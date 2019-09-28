cc83403bd377673e6a4cdd322f3966561db80970
Quinten Kock <quintenkock@gmail.com>
2019-09-28 18:16:48 +0200

    Implement Entry API and some more for TupleMap

--- src/tuplemap.rs
+++ src/tuplemap.rs
@@ -1,4 +1,4 @@
-
+#[derive(Clone)]
 pub struct TupleMap<Key, Value> {
     pairs: Vec<(Key, Value)>,
 
@@ -33,5 +33,60 @@ impl<Key: PartialEq, Value> TupleMap<Key, Value> {
             },
             None => {}
         }
+    }
+
+    pub fn len(&self) -> usize {
+        self.pairs.len()
+    }
+    pub fn iter(&self) -> std::slice::Iter<(Key, Value)> {
+        self.pairs.iter()
+    }
+    pub fn entry(&mut self, k: Key) -> TupleMapEntry<Key, Value> {
+        TupleMapEntry {
+            parent: self,
+            k: k
+        }
+    }
+}
+
+pub struct TupleMapEntry<'a, Key, Value> {
+    parent: &'a mut TupleMap<Key, Value>,
+    k: Key,
+}
+impl<'a, Key: PartialEq, Value> TupleMapEntry<'a, Key, Value> {
+    pub fn or_insert(self, default: Value) -> &'a mut Value {
+        match self.parent.find(&self.k) {
+            Some(i) => &mut self.parent.pairs[i].1,
+            None => {
+                self.parent.pairs.push((self.k, default));
+                &mut self.parent.pairs.last_mut().unwrap().1
+            }
+        }
+    }
+    pub fn or_insert_with<F: FnOnce() -> Value>(self, default: F) -> &'a mut Value {
+        match self.parent.find(&self.k) {
+            Some(i) => &mut self.parent.pairs[i].1,
+            None => {
+                self.parent.pairs.push((self.k, default()));
+                &mut self.parent.pairs.last_mut().unwrap().1
+            }
+        }
+
+    }
+    pub fn and_modify<F: FnOnce(&mut Value)>(self, f: F) -> Self {
+        match self.parent.find(&self.k) {
+            Some(i) => f(&mut self.parent.pairs[i].1),
+            None => {}
+        }
+        self
+    }
+    pub fn key(&self) -> &Key {
+        &self.k
+    }
+}
+
+impl<'a, Key: PartialEq, Value: Default> TupleMapEntry<'a, Key, Value> {
+    pub fn or_default(self) -> &'a mut Value {
+        self.or_insert_with(Value::default)
     }
 }
