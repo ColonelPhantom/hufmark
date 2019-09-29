94072cfc39c449fa02688cc9db615461adc3aed3
Quinten Kock <quintenkock@gmail.com>
2019-09-30 01:39:35 +0200

    Reduce hashtable reserve

--- src/main.rs
+++ src/main.rs
@@ -29,7 +29,7 @@ fn main() {
     // let text = text_str.chars();
 
     let mut hist = History::new(HIST_LEN);
-    let mut markov = Markov::with_capacity(HIST_LEN/2 * text.len());
+    let mut markov = Markov::with_capacity(HIST_LEN/4 * text.len());
     // let mut markov = Markov::new();
     let mut correct = [0; PREDICT_TRIES];
     let mut wrong = 0;
@@ -60,7 +60,7 @@ fn main() {
 
     println!("Table pressure: {}/{}", markov.get_len(), markov.get_capacity());
 
-    // std::io::stdin().read_line(&mut String::new());
+    std::io::stdin().read_line(&mut String::new());
 
     println!("MarkovValue size: {}", std::mem::size_of::<markov::MarkovValue<char>>());
     println!("OptionVec size: {}", std::mem::size_of::<Option<Vec<char>>>());
